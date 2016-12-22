23576d01da81a32789911bfca0edb6e1e16c328e
Mat Moore <matmoore@users.noreply.github.com>
2016-12-22 21:16:17 +0000

    Hello world

---
+++ README.md
@@ -0,0 +1,5 @@
+# Search Engine
+
+A mini search index.
+
+Built to learn golang and/or [information retrieval](http://nlp.stanford.edu/IR-book/pdf/irbookonlinereading.pdf).
---
+++ index.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+import "strings"
+import "sort"
+
+const doc1 = "I did enact Julius Caesar: I was killed i’ the Capitol; Brutus killed me."
+
+const doc2 = "So let it be with Caesar. The noble Brutus hath told you Caesar was ambitious:"
+
+type Index struct {
+	terms map[string][]string
+}
+
+func tokenise(text string) []string {
+	return strings.Fields(text)
+}
+
+func (index Index) insertPosting(term string, docId string) {
+	existing := index.terms[term]
+	withDoc := append(existing, docId)
+	sort.Strings(withDoc)
+	index.terms[term] = withDoc
+}
+
+func (index Index) Index(docId string, text string) {
+	tokens := tokenise(text)
+	for _, term := range tokens {
+		index.insertPosting(term, docId)
+	}
+}
+
+func New() Index {
+	return Index{terms: make(map[string][]string)}
+}
+
+func (index Index) Show() string {
+	var result string
+	for key, value := range index.terms {
+		result += fmt.Sprintf("%s -> %s\n", key, strings.Join(value, ", "))
+	}
+	return result
+}
+
+func main() {
+	index := New()
+	index.Index("doc1", doc1)
+	index.Index("doc2", doc2)
+	fmt.Println("WELCOME TO GOOGLE 2.0 🔍 ")
+	fmt.Println("------------------------")
+	fmt.Print(index.Show())
+}
