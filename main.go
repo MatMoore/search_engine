0bf28ef3b4100eab47e24625527b10b0b23c8160
Mat Moore <matmoore@users.noreply.github.com>
2016-12-23 23:45:44 +0000

    Input

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
+import "bufio"
 import "fmt"
+import "os"
 import "github.com/matmoore/search_engine/search"
 
 const doc1 = "I did enact Julius Caesar: I was killed i’ the Capitol; Brutus killed me."
@@ -11,8 +13,18 @@ func main() {
 	index := search.NewIndex()
 	index.Index("doc1", doc1)
 	index.Index("doc2", doc2)
+
 	fmt.Println("WELCOME TO GOOGLE 2.0 🔍 ")
 	fmt.Println("------------------------\n")
 	fmt.Print(index.Show())
-	fmt.Printf("Results: %#v\n", index.Search("julius AND caesar"))
+
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("> ")
+	for scanner.Scan() {
+		query := scanner.Text()
+		fmt.Printf("Results: %#v\n", index.Search(query))
+		fmt.Print("> ")
+	}
+
+	fmt.Println("Bye!")
 }
